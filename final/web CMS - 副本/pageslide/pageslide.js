42f1c7c872332d91f42e452f7ac9a7bb2c5b670b
BobTuan <858046022@qq.com>
2020-05-12 20:11:23 +0800

    阅读界面

---
+++ final/web CMS - 副本/pageslide/pageslide.js
@@ -0,0 +1,14 @@
+/* Default pageslide, moves to the right */
+$jjq("#treeSlide").pageslide({ direction: "right", modal: true });
+$jjq("#tagSlide").pageslide({ direction: "right", modal: true });
+$jjq("#rssSlide").pageslide({ direction: "right", modal: true });
+$jjq("#manageContainer").click(function(e){
+    if (e.target.id!="treeSlide" || e.target.id!="tagSlide"){
+            $jjq.pageslide.close();
+    };
+})
+$jjq("#rssContainer").click(function(e){
+    if (e.target.id!="rssSlide"){
+            $jjq.pageslide.close();
+    };
+})
